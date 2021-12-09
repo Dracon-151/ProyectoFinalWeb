869bcc4f3c34ca8ca5d05da2fa36f5df00038dc8
EduardoGonzalezV024 <jog2_19@alu.uabcs.mx>
2021-12-08 19:32:29 -0600

    cambios bootstrap

--- Index.php
+++ Index.php
@@ -1,9 +1,26 @@
 ﻿<html lang = "en">
 <head>
-	<meta charset ="UTF-8">
+	<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#bla"  />
+
 	<title>Estim</title>
+
     <script src="bootstrap/js/bootstrap.js"></script>
+
+	<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
+
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style2.css">
+
 	<?php 
 		session_start();	
 		if(isset($_SESSION['idSesion'])){
@@ -15,47 +32,50 @@
 
 	<link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-	<a class="logo__index" href="Index.html">a</a>
+		<a class="logo__index" href="Index.html">a</a>
+
 	<header class ="header">
 		<form action ="">
-				<div class = "barra-superior">
-					<img src="img/logo.png" class ="logo">
-					<input type = "text" id ="task">
-					<button class = "btn btn-search" id="btnSearch">
-						<i class="fas fa-search"></i>
-					</button>
-					<?php 
-						if(!isset($usuario)){
-							echo "<a href = 'Login.php' class = 'login'>Iniciar sesión</a>";
-						}else{
-							echo "<a href = 'IniciodeSesion/CerrarSesion.php'class = 'login'>Cerrar sesión</a>";
-						}
-	 				?>
-					<a href = "Carrito.php">
-                        <i class="icon icon-cart fas fa-shopping-cart"></i>
-                    </a>
-				</div>
+			<div class = "barra-superior">
+				<img src="img/logo.png" class ="logo">
+				<input type = "text" id ="task">
+				<button class = "btn btn-search" id="btnSearch">
+					<i class="fas fa-search"></i>
+				</button>
+				<a href = "Login.html" class = "login">
+			    Iniciar sesión</a>
+			</div>
 		</form>
 	</header>
-	<div class ="barra-inferior">
-			<div class = "barra-inferior">
-				<a href = "Productos.php" class = "categoria">
-				PlayStation</a>
-				<a href = "Productos.php" class = "categoria">
-				Xbox</a>
-				<a href = "Productos.php" class = "categoria">
-				Nintendo</a>
-				<a href = "Productos.php" class = "categoria">
-				PC</a>
-				<a href = "Productos.php" class = "categoria">
-				VR</a>
-				<a href = "Productos.php" class = "categoria">
-				Ofertas</a>
-				<a href = "EnviarComentarios.php" class = "categoria">
-				Contáctanos</a>
-			</div>
-	</div>
+
+	<nav class="navbar navbar-inverse" role="navigation" style="background-color: #83BA7E;">
+	  <div class="container">
+
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-nav" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	    </div>
+
+	    <div class="collapse navbar-collapse" id="bs-nav">
+	      <ul class="nav navbar-nav">
+	        <li><a href="Productos.html">PlayStation</a></li>
+	        <li><a href="Productos.html">Xbox</a></li>
+	        <li><a href="Productos.html">Nintendo</a></li>
+	        <li><a href="Productos.html">PC</a></li>
+	        <li><a href="Productos.html">VR</a></li>
+	        <li><a href="Productos.html">Ofertas</a></li>
+	        <li><a href="Productos.html">Contáctanos</a></li>
+	        <li><a href="Carrito.html"><i class="icon icon-cart fas fa-shopping-cart"></i></a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
 
 	<main class="content">
 		<img src="img/publicidad.png" class ="img_publicidad" width="1366" height="182" />
@@ -142,6 +162,18 @@
 			</section>
 		</div>
 	</main>
+	
+	<script src="https://code.jquery.com/jquery-3.6.0.js"
+  	integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  	crossorigin="anonymous"></script>
+
+  	<!-- JavaScript Bundle with Popper -->
+  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+  	integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+  	crossorigin="anonymous"></script>
+
+	<script src="./js/scripts.js"></script>
+
 	<?php
     if(isset($_REQUEST["btnAgregar"])){
         if(isset($_SESSION['idSesion'])){
@@ -171,5 +203,7 @@
 	    }
     }
     ?>
+
+    
 </body>
 </html>
